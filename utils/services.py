391957531a565d96940d5b17de013f640212aec4
Renat <matador.99@mail.ru>
2023-05-12 20:53:05 +0500

    "function load_json"

--- utils/services.py
+++ utils/services.py
@@ -1,11 +1,17 @@
 import json
+import os.path
+
+PATH_TO_DATA = os.path.abspath(os.path.join("..", "data"))
+PATH_TO_OPERATION_JSON = os.path.join("..", PATH_TO_DATA, 'operation.json')
 
 
-file_ = '../question/questions.json'
 
 
-def load_json(file_path):
-    with open('file_path', 'r') as f:
+
+def load_json():
+    with open(PATH_TO_OPERATION_JSON, 'r') as f:
         date = json.load(f)
-    return date
+    print(date)
 
+
+load_json()
