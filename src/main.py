f1e1bbb096fba9e12442f0a5a96a900d3cb104fb
Renat <matador.99@mail.ru>
2023-05-22 18:20:15 +0500

    "main()"

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 from utils.services import load_json, get_last_five_successful_operations, sort_by_date, print_info, filename
 
-if __name__ == '__main__':
+
+def main():
     json_data = load_json(filename)
     sorted_operation = sort_by_date(json_data)
     last_five_operations = get_last_five_successful_operations(sorted_operation)
     print_info(last_five_operations)
+
+
+if __name__ == '__main__':
+    main()
